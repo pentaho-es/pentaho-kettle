9b0a9cc653782d2125de4b4fa447ee55e357ca69
Bruno Costa <bruno.costa@hitachivantara.com>
2023-11-13 16:08:29 +0000

    [SP-6464] - Backport of PDI-19805 - The Single Threader step will not process rows correctly for certain steps (9.3 Suite)
    (cherry picked from commit 143d8fee955278a893b0492d5ff87c10fdc5e473)
    
    [PDI-19805] - The Single Threader step will not process rows correctly for certain steps
    
    (cherry picked from commit 9a26335c1f272282ad7d8fe28fd9ee5e3f21f4f9)
    
    [PDI-19805] - The Single Threader step will not process rows correctly for certain steps
    
    (cherry picked from commit a755bdcd9e917d385b0e324f95f0b0d5d16003c1)

--- plugins/excel/core/src/main/java/org/pentaho/di/trans/steps/excelwriter/ExcelWriterStep.java
+++ plugins/excel/core/src/main/java/org/pentaho/di/trans/steps/excelwriter/ExcelWriterStep.java
@@ -212,12 +212,7 @@ public class ExcelWriterStep extends BaseStep implements StepInterface {
       }
       return true;
     } else {
-      // after the last row, the (last) file is closed
-      if ( data.wb != null ) {
-        closeOutputFile();
-      }
       setOutputDone();
-      clearWorkbookMem();
       return false;
     }
   }
@@ -229,6 +224,21 @@ public class ExcelWriterStep extends BaseStep implements StepInterface {
     data.wb = null;
     data.clearStyleCache( 0 );
 
+  }
+
+  @Override
+  public boolean afterFinishProcessing( StepMetaInterface smi, StepDataInterface sdi ) {
+    if ( data.wb != null ) {
+      try {
+        closeOutputFile();
+      } catch ( KettleException e ) {
+        logDetailed( e.getMessage() );
+        return false;
+      }
+    }
+    setOutputDone();
+    clearWorkbookMem();
+    return true;
   }
 
   private void closeOutputFile() throws KettleException {
@@ -261,6 +271,7 @@ public class ExcelWriterStep extends BaseStep implements StepInterface {
       }
 
       data.wb.write( out );
+      data.wb = null;
     } catch ( IOException e ) {
       throw new KettleException( e );
     }
